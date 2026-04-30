0fe4da7b95f6073de819fa993e16ce4f97d070b4
Matusal3m <matusalem.dev@gmail.com>
2026-04-30 14:16:33 -0300

    telescope find files now show hidden files AND hide .git

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -4,13 +4,13 @@ local builtin = require('telescope.builtin')
 require('telescope').setup {
     pickers = {
         find_files = {
-            hidden = true
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
         }
     }
 }
 
 map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-map('n', '<leader>fg', builtin.live_grep,  { desc = 'Telescope live grep' })
-map('n', '<leader>fb', builtin.buffers,    { desc = 'Telescope buffers' })
-map('n', '<leader>fh', builtin.help_tags,  { desc = 'Telescope help tags' })
-
+map('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+map('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+map('n', '<leader>gs', builtin.git_status, { desc = 'Telescope git status' })
